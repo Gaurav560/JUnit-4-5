89713610eba626b4ddcfd226b3c6e45d870f35f0
GAURAV SHARMA <gsharma1154@gmail.com>
2023-12-26 00:38:24 +0530

    @BeforeClass and @AfterClass for running before and after test cases

--- UnitTestingWithJunit4and5/src/test/java/com/ineuron/ai/service/CalculatorServiceTest.java
+++ UnitTestingWithJunit4and5/src/test/java/com/ineuron/ai/service/CalculatorServiceTest.java
@@ -1,14 +1,37 @@
 package com.ineuron.ai.service;
 
+import java.util.Date;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorServiceTest {
 
+	//saare test cases chalne se agar hm koi logic run karna chahte hain to 
+	//hm use karenge @BeforeClass and agar saare test cases pass hone ke baad 
+	//agar hm koi logic run karna chahte hain to @AfterClass.
+	//aur haan dono method class level ki hogi ,means static hogi 
 	
-	//test method to ad  two numbers
+	@AfterClass
+	public static void cleanUpLogic() {
+		System.out.println("After all test Cases:");
+		System.out.println("ended test"+new Date());
+	}
+	
+	@BeforeClass
+	public static void intialize() {
+		System.out.println("before all test cases:");
+		System.out.println("started test"+new Date());
+	}
+	
+	
+	//test method to add  two numbers
 	
 	@Test
 	public void addTwoNumberTest() {
+		System.out.println("execution thread is in addTwoNumberTest Method");
 		int result=CalculatorService.addTwoNumbers(12, 13);
 		
 		int expectedResult=25;
@@ -20,4 +43,16 @@ public class CalculatorServiceTest {
 		
 		
 	}
+	
+	
+	@Test
+	public void sumAnyNumbersTest() {
+		System.out.println("execution thread is in sumAnyNumbersTest Method");
+	int result=CalculatorService.sumAnyNumbers(3,45,6,7,4);
+	
+	int expectedResult=65;
+	Assert.assertEquals(expectedResult, result);
+	}
+	
+
 }
